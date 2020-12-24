0311f979983d0da43a6083247f67c708e2bb5ee1
yukimi.y <yyy.yukimi@gmail.com>
2020-12-24 15:33:21 +0900

    投稿一覧機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_225255) do
+ActiveRecord::Schema.define(version: 2020_12_24_061553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.first
+Item.create!(name: "ホームズ", date: '2020-10-14', user_id: user.id)
+Item.create!(name: "赤と黒", date: '2020-11-04', user_id: user.id)
+Item.create!(name: "赤毛のアン", date: '2020-12-20', user_id: user.id)
+puts "初期データの投入に成功しました！"
