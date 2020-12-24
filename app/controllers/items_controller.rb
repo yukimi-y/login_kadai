db97a86caf7bb9cf2898dff4a77f24dcaeef0593
yukimi.y <yyy.yukimi@gmail.com>
2020-12-24 15:41:31 +0900

    新規投稿機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,22 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: :index
+
   def index
     @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create!(name: item_params[:name], date: item_params[:date], user_id: current_user.id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :date)
   end
 end
